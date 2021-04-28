0c6354b4a44ada7dac8493b9dd63750526fa5f5a
yd <664720125@qq.com>
2021-04-28 09:46:22 +0800

    1 Signed-off-by: yd <664720125@qq.com>

--- src/org/devops/tool.groovy
+++ src/org/devops/tool.groovy
@@ -1,5 +1,11 @@
 package org.devops
 
-def printMsg(contains){
-  print(contains)
+def printMsg(contains,color){
+    colors = ['red'   : "\033[40;31m >>>>>>>>>>>${contains}<<<<<<<<<<< \033[0m",
+              'blue'  : "\033[47;34m ${contains} \033[0m",
+              'green' : "[1;32m>>>>>>>>>>${contains}>>>>>>>>>>[m",
+              'green1' : "\033[40;32m >>>>>>>>>>>${contains}<<<<<<<<<<< \033[0m" ]
+    ansiColor('xterm') {
+        println(colors[color])
+    }
 }
