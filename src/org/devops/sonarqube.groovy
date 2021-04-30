5d8ad14bc76cffe16d1832fe313c83bf0f1ce42a
yd <664720125@qq.com>
2021-05-01 00:01:54 +0800

    Signed-off-by: yd <664720125@qq.com>

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -13,9 +13,12 @@ def SonarScan(sonarServer,projectName,projectDesc,projectPath,branchName){
         sonarServer = "http://134.175.149.69:9000"
         def sonarDate = sh  returnStdout: true, script: 'date  +%Y%m%d%H%M%S'
         sonarDate = sonarDate - "\n"
-    
-        
-        sh """ 
+		print("扫描命令：${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
+            -Dsonar.projectName=${projectName} -Dsonar.projectVersion=${sonarDate} -Dsonar.ws.timeout=30 \
+            -Dsonar.projectDescription=${projectDesc} -Dsonar.links.homepage=http://www.baidu.com \
+            -Dsonar.sources=${projectPath} -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=target/classes \
+            -Dsonar.java.test.binaries=target/test-classes -Dsonar.java.surefire.report=target/surefire-reports  -Dsonar.branch.name=${branchName} -X")
+        sh """  
             ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
             -Dsonar.projectName=${projectName} -Dsonar.projectVersion=${sonarDate} -Dsonar.ws.timeout=30 \
             -Dsonar.projectDescription=${projectDesc} -Dsonar.links.homepage=http://www.baidu.com \
