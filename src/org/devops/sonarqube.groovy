4ca1d6e203d44174a5bf036906afcecd05a54a24
yd955213 <664720125@qq.com>
2021-05-02 00:44:24 +0800

    Update sonarqube.groovy

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -13,11 +13,7 @@ def sonarScan1(sonarServer,projectName,projectDesc,projectPath,branchName){
         //sonarServer = "http://134.175.149.69:9000"
         def sonarDate = sh  returnStdout: true, script: 'date  +%Y%m%d%H%M%S'
         sonarDate = sonarDate - "\n"
-		print("扫描命令：${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
-            -Dsonar.projectName=${projectName} -Dsonar.projectVersion=${sonarDate} -Dsonar.ws.timeout=30 \
-            -Dsonar.projectDescription=${projectDesc} -Dsonar.links.homepage=http://www.baidu.com \
-            -Dsonar.sources=${projectPath} -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=target/classes \
-            -Dsonar.java.test.binaries=target/test-classes -Dsonar.java.surefire.report=target/surefire-reports  -Dsonar.branch.name=${branchName} -X")
+
         sh """  
             ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
             -Dsonar.projectName=${projectName} -Dsonar.projectVersion=${sonarDate} -Dsonar.ws.timeout=30 \
